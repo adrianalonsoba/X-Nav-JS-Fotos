442061ac377f2d514265fe2a8a3acf44b1965027
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-02-02 14:19:17 +0100

    añadidas urls

---
+++ script.js
@@ -0,0 +1,3 @@
+var urls=['http://upload.wikimedia.org/wikipedia/commons/5/59/Thyssen-Krupp-Quartier-Essen-Q1-2013.jpg','http://commons.wikimedia.org/wiki/Category:Computer_generated_images#mediaviewer/File:4-cube_5^4.png','http://commons.wikimedia.org/wiki/Category:Computer_generated_images#mediaviewer/File:4-cube_4^4.png','http://commons.wikimedia.org/wiki/Category:Computer_generated_images#mediaviewer/File:Bedroom_%286749182873%29.jpg'];
+
+
