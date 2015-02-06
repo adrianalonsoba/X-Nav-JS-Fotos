2b0291c17d34d36e57f27079ed9dda04b07968bb
adrianalonsoba <adrian.alonsoba@gmail.com>
2015-02-06 19:30:01 +0100

    añadidas mas fotos

--- photos.js
+++ photos.js
@@ -1,5 +1,9 @@
 var urls=['http://www.trecebits.com/wp-content/uploads/2014/06/Google-Moves-Into.png',
 'http://assets.worldwildlife.org/photos/2325/images/hero_small/mountains-hero.jpg?1345838509',
+'http://media-cdn.tripadvisor.com/media/photo-s/03/d0/66/5c/playa-bacocho.jpg',
+'http://vignette2.wikia.nocookie.net/kailiaukisland/images/8/8a/Jungle1.jpeg/revision/latest?cb=20100608024535',
+'http://gambargadget.info/wp-content/uploads/2014/06/galaxy-tumblr-photography.jpg',
+'http://asusta2.com.ar/wp-content/uploads/2008/07/03-piramide-de-keops.jpg'
 ];
 
 function changer (id, newValue) {
@@ -8,6 +12,6 @@ function changer (id, newValue) {
 }
 
 function change(){
-	var index=Math.floor((Math.random() * 2));
+	var index=Math.floor((Math.random() * urls.length));
 	changer('photo','<img src='+urls[index]+'>');
 }
